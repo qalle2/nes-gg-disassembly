14ace139b4f4cdc9509efb0e1f78b00592c8fe20
Kalle <qalle85@gmail.com>
2018-08-15 20:46:07 +0300

    don't delete files, user-friendlier, error msgs

--- assemble.bat
+++ assemble.bat
@@ -1,20 +1,48 @@
 @echo off
 cls
-rem delete original prg.bin (required to detect assembly failure;
-rem Ophis does not seem to support errorlevel)
-if exist prg.bin del prg.bin
-rem assemble PRG-ROM to prg.bin, be verbose
+
+rem user must have original CHR-ROM
+if not exist original-chr.bin goto nochr
+
+echo === assemble.bat: assembling ===
+echo.
 ophis -v -o prg.bin prg.asm
-rem exit if assembly failed
-if not exist prg.bin goto error
-rem compare original PRG-ROM (not included) to assembled file
-fc /b original-prg.bin prg.bin
-rem assemble entire file to gg.nes, be verbose
+echo.
+if errorlevel 1 goto assemblyerror
 ophis -v -o gg.nes gg.asm
-rem compare original file (not included) to assembled file
+echo.
+if errorlevel 1 goto assemblyerror
+
+rem user must have original PRG-ROM and entire ROM to compare
+if not exist original-prg.bin goto cannotcompare
+if not exist original.nes goto cannotcompare
+
+echo === assemble.bat: comparing original files to assembled files ===
+echo.
+fc /b original-prg.bin prg.bin
+echo.
+if errorlevel 2 goto fcerror
 fc /b original.nes gg.nes
+echo.
+if errorlevel 2 goto fcerror
+
 rem success
 goto end
-:error
-echo assemble.bat: error detected!
+
+:nochr
+echo assemble.bat: error: "original-chr.bin" not found; see readme for how to get it
+goto end
+
+:assemblyerror
+echo assemble.bat: error: assembly failed
+goto end
+
+:cannotcompare
+echo assemble.bat: warning: cannot compare to original files ("original-prg.bin", "original.nes") because they do not exist
+goto end
+
+:fcerror
+echo assemble.bat: error: FC returned an error
+goto end
+
 :end
